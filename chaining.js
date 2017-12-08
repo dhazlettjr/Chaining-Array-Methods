80b3b913214e6c5d10bc3817c83c37b666415ed2
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-08 15:40:47 -0600

    finished .map and .reduce to get final number

--- chaining.js
+++ chaining.js
@@ -1,5 +1,11 @@
+// start the project with an array of integers
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-(integers.sort(function(a, b){return b-a})
 
+// make a single line of code to complete the task
+// make the array output in descending order
+// add a filter to only keep anything 19 and below
+// create a function that will return less than or equal to 19
+
+console.log(integers.sort(function(a, b){return b-a}).filter(function (cut){return cut<=19}).map(function(num){return num * 1.5 - 1}).reduce(function(a, b){return a+b}))
 
 
